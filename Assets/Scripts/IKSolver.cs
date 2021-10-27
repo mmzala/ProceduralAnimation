6bb2adf7d26e0c9bb246fbf619b5f1325eb7cdf1
Marcin Zalewski <marcinzal24@gmail.com>
2021-10-27 10:35:37 +0200

    IKSolver.cs refactoring

--- Assets/Scripts/IKSolver.cs
+++ Assets/Scripts/IKSolver.cs
@@ -44,7 +44,9 @@ public class IKSolver
     {
         // Get joint positions to not do any computations on the joints directly
         for (int i = 0; i < joints.Length; i++)
+        {
             pos[i] = joints[i].position;
+        }
 
         // Check if joints are long enough to reach the target
         if ((target - joints[0].position).sqrMagnitude >= totalLength * totalLength)
@@ -54,7 +56,7 @@ public class IKSolver
         }
         else
         {
-            Iterate(target);
+            SolveIK(target);
             MoveTowardsPole();
         }
 
@@ -71,22 +73,26 @@ public class IKSolver
         }
     }
 
-    void Iterate(Vector3 target)
+    void SolveIK(Vector3 target)
     {
         for (int i = 0; i < iterations; i++)
         {
             // Backward reaching
             pos[pos.Length - 1] = target;
             for (int j = pos.Length - 2; j > 0; j--)
+            {
                 pos[j] = pos[j + 1] + (pos[j] - pos[j + 1]).normalized * jointsDist[j];
+            }
 
             // Forward reaching
             for (int j = 1; j < pos.Length; j++)
+            {
                 pos[j] = pos[j - 1] + (pos[j] - pos[j - 1]).normalized * jointsDist[j - 1];
+            }
+                
 
             // If is close enough to target, break
-            if ((pos[pos.Length - 1] - target).sqrMagnitude < delta * delta)
-                break;
+            if ((pos[pos.Length - 1] - target).sqrMagnitude < delta * delta) break;
         }
     }
 
@@ -98,11 +104,14 @@ public class IKSolver
         {
             // Create a plane perpendicular to vector between next and last bone, on the last bone
             Plane plane = new Plane(pos[i + 1] - pos[i - 1], pos[i - 1]);
+
             // Get projected pole and joint position on the plane
             Vector3 projectedPole = plane.ClosestPointOnPlane(pole.position);
             Vector3 projectedJoint = plane.ClosestPointOnPlane(pos[i]);
+
             // Calculate angle we have to move the joint
             float angle = Vector3.SignedAngle(projectedJoint - pos[i - 1], projectedPole - pos[i - 1], plane.normal);
+
             // Rotate the joint around the axis on the plane normal, and set it back in position
             pos[i] = Quaternion.AngleAxis(angle, plane.normal) * (pos[i] - pos[i - 1]) + pos[i - 1];
         }
@@ -115,6 +124,8 @@ public class IKSolver
     {
         Vector3 dir = (target - pos[0]).normalized;
         for (int i = 1; i < pos.Length; i++)
+        {
             pos[i] = pos[i - 1] + dir * jointsDist[i - 1];
+        }
     }
 }
